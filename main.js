7a5ecc38c02d73bc7ecc8067bef72acc43397abe
Daniel Rajakumar <danielelvisr@gmail.com>
2023-05-26 13:24:25 -0400

    push

--- main.js
+++ main.js
@@ -7,33 +7,10 @@ darkReaderScript.referrerPolicy = 'no-referrer';
 darkReaderScript.onload = () => DarkReader.enable();
 document.head.insertBefore(darkReaderScript, document.head.firstChild);
 
-// /html/body/div[2]/div/section
-
-
-// document.querySelector("#home")
-
 function loadGIF(){
   let element = document.getElementById("home")
   element.style.backgroundImage = 'url("' + 'https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExMDZkZmYzOGNhNDE3NDE4M2RiNTQxMmUxMTM5YWZhYjVmZjdjMzA4NyZlcD12MV9pbnRlcm5hbF9naWZzX2dpZklkJmN0PWc/xT9IgzoKnwFNmISR8I/giphy.gif' + '")';
   // element.style.opacity = 0.2;
 }
 
-
-// Code to be executed every second
-function runEverySecond() {
-  // Your code here
-  // DarkReader.enable();
-
-  if (DarkReader.isEnabled()) {
-      // clearInterval(interval); // Stop the interval timer
-  }
-    
-}
-
-// Start the interval timer
 loadGIF();
-// let interval = setInterval(runEverySecond, 1);
-
-// this is a update
-
-// DarkReader.enable();
